aeb2c40b664165052d552f2cce9531e7041ac2fc
Abdulsami08 <abdulsamishaikh08@gmail.com>
2024-07-09 12:44:15 +0500

    Update route.ts

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,7 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
+import { db } from "@vercel/postgres";
+import { error } from "console";
 
+export async function GET(request: NextRequest){
+    const client = await db.connect();
+    
+    try{
+        await client.sql`CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255))`
+        const res = client.sql`SELECT * FROM todos`
+        console.log(res)
+        return NextResponse.json({data:  res})
+    }catch(err){
+        console.log(err)
+        return NextResponse.json({message: "something went wrong"})
+    }
+}
 
-export function GET(request: NextRequest){
+export async function POST(request: NextRequest){
+        const req = await request.json();
 
-    return NextResponse.json({message: "You call this api"})
+        try {
+            if(req.task){
+                const client = await db.connect();
+                const res = await client.sql `INSERT INTO Todos(Task) VALUES (${req.task})`
+                console.log(res)
+                return NextResponse.json({message: "Data added successfully"})
+            }
+            else{
+                throw new Error("Task is required")
+            }
+        } catch (error) {
+            return NextResponse.json({message: (error as {message:string}).message})
+        }
 }
